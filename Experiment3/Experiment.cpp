518494bd09039e16a9b10da3cc852b6fb84a64fb
aryan0416 <aryandahiya0416@gmail.com>
2026-01-29 14:55:03 +0530

    24BAI70697
    Implement N-Queens solution using backtracking.

--- Experiment3/Experiment.cpp
+++ Experiment3/Experiment.cpp
@@ -1,1 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> result;
+vector<string> board;
+int n;
+
+vector<int> colmn, diag1, diag2;
+
+bool isSafe(int x, int y) {
+    if(colmn[y] || diag1[x - y + n - 1] || diag2[x + y]) return false;
+    return true;
+}
+
+void solve(int row) {
+    if (row == n) {
+        result.push_back(board);
+        return;
+    }
+
+    for (int col = 0; col < n; col++) {
+        if (isSafe(row, col)) {
+            board[row][col] = 'Q';
+
+            colmn[col] = diag1[row - col + n - 1] = diag2[row + col] = 1;
+
+            solve(row + 1);
+
+            board[row][col] = '.';
+            colmn[col] = diag1[row - col + n - 1] = diag2[row + col] = 0;
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+
+    board.assign(n, string(n, '.'));
+    
+    colmn.assign(n, 0);
+    diag1.assign(2*n, 0);
+    diag2.assign(2*n, 0);
+
+    solve(0);
+  
+    for (auto &sol : result) {
+        for (auto &row : sol)
+            cout << row << endl;
+        cout << "------\n";
+    }
+
+    return 0;
+}
+
+// Time complexity : n!    for isSafe : O(1)
 
